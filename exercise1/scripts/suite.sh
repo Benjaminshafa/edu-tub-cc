9fea16909583b1e6332086dc826c8639f8613111
Till Rohrmann <till.rohrmann@googlemail.com>
2012-05-04 20:59:54 +0200

    minor changes

--- exercise1/scripts/suite.sh
+++ exercise1/scripts/suite.sh
@@ -13,15 +13,20 @@ EXECLIN=linpack.sh
 EXECMEMSWEEP=memsweep.sh
 EXECSYSCALL=syscall.sh
 EXECFORK=fork.sh
-DATE=$(date +%m-%d-%y_%T)
+DATE=$(date +%m-%d-%y)
+
+SUFLIN=$(ls ${OUTLIN}*.out 2>/dev/null | wc -l)
+SUFMEMSWEEP=$(ls ${OUTMEMSWEEP}*.out 2>/dev/null | wc -l)
+SUFSYSCALL=$(ls ${OUTSYSCALL}*.out 2>/dev/null | wc -l)
+SUFFORK=$(ls ${OUTFORK}*.out 2>/dev/null | wc -l)
 
 OLD=$(pwd)
 
 cd ${DST}
-${OLD}/execn.sh ${NUMLIN} ./${EXECLIN} | ${OLD}/timestamp.sh | tee ${OLD}/${OUTLIN}-${DATE}.out
-${OLD}/execn.sh ${NUMMEMSWEEP} ./${EXECMEMSWEEP} | ${OLD}/timestamp.sh | tee ${OLD}/${OUTMEMSWEEP}-${DATE}.out
-${OLD}/execn.sh ${NUMSYSCALL} ./${EXECSYSCALL} | ${OLD}/timestamp.sh | tee ${OLD}/${OUTSYSCALL}-${DATE}.out
-${OLD}/execn.sh ${NUMFORK} ./${EXECFORK} | ${OLD}/timestamp.sh | tee ${OLD}/${OUTFORK}-${DATE}.out
+${OLD}/execn.sh ${NUMLIN} ./${EXECLIN} | ${OLD}/timestamp.sh | tee ${OLD}/${OUTLIN_${DATE}_${SUFLIN}.out
+${OLD}/execn.sh ${NUMMEMSWEEP} ./${EXECMEMSWEEP} | ${OLD}/timestamp.sh | tee ${OLD}/${OUTMEMSWEEP}_${DATE}_${SUFMEMSWEEP}.out
+${OLD}/execn.sh ${NUMSYSCALL} ./${EXECSYSCALL} | ${OLD}/timestamp.sh | tee ${OLD}/${OUTSYSCALL}_${DATE}_${SUFSYSCALL}.out
+${OLD}/execn.sh ${NUMFORK} ./${EXECFORK} | ${OLD}/timestamp.sh | tee ${OLD}/${OUTFORK}_${DATE}_${SUFFORK}.out
 
 cd ${OLD}
 
